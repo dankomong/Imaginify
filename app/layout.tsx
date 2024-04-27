2cce4da51553f6e8ba4379e3625faf9bfea3a8e5
Daniel <dankomong@gmail.com>
2024-04-27 19:02:30 -0400

    Implement nav bar for web and mobile view

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 
@@ -26,11 +26,6 @@ export default function RootLayout({
     }}>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-          <header>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-          </header>
           {children}
         </body>
       </html>
